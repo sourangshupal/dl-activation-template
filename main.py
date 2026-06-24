98e4506afc0dc82b04e09b8be6f57f7dc79e365a
Sourangshu Paul <14earcs0158@aryacollege.in>
2026-06-24 17:07:59 +0530

    Add starter code

---
+++ main.py
@@ -0,0 +1,21 @@
+import math
+
+
+def sigmoid(x: float) -> float:
+    """TODO: implement sigmoid activation: 1 / (1 + e^-x)"""
+    pass
+
+
+def relu(x: float) -> float:
+    """TODO: implement ReLU activation: max(0, x)"""
+    pass
+
+
+def tanh_activation(x: float) -> float:
+    """TODO: implement tanh activation using math.tanh"""
+    pass
+
+
+def softmax(values: list[float]) -> list[float]:
+    """TODO: implement softmax over a list of values"""
+    pass
